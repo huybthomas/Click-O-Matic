5cb16ca8858234e0c735a314f0366dbf5c369384
Thomas Huybrechts <huybthomas@gmail.com>
2015-10-10 20:30:07 +0200

    Upload test

--- src/main/java/be/uantwerpen/iw/ei/se/ClickOMaticApplication.java
+++ src/main/java/be/uantwerpen/iw/ei/se/ClickOMaticApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClickOMaticApplication {
-
-    public static void main(String[] args) {
+public class ClickOMaticApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(ClickOMaticApplication.class, args);
     }
-    //als je dit ziet werkt het
 }
