b1ac262dca7413e51c72175c30f9b7e72be7cfbb
Thomas Huybrechts <huybthomas@gmail.com>
2015-10-10 18:25:48 +0200

    Initial project commit

---
+++ src/main/java/be/uantwerpen/iw/ei/se/ClickOMaticApplication.java
@@ -0,0 +1,12 @@
+package be.uantwerpen.iw.ei.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickOMaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickOMaticApplication.class, args);
+    }
+}
---
+++ src/test/java/be/uantwerpen/iw/ei/se/ClickOMaticApplicationTests.java
@@ -0,0 +1,18 @@
+package be.uantwerpen.iw.ei.se;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClickOMaticApplication.class)
+@WebAppConfiguration
+public class ClickOMaticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
