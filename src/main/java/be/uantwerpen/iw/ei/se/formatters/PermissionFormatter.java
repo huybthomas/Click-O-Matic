d459713841c9a349d13b22bd69fde1a4c014268a
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-02 12:45:23 +0100

    Implemented formatters:  + PermissionFormatter  + RoleFormatter

--- src/main/java/be/uantwerpen/iw/ei/se/formatters/PermissionFormatter.java
+++ src/main/java/be/uantwerpen/iw/ei/se/formatters/PermissionFormatter.java
@@ -1,7 +1,37 @@
 package be.uantwerpen.iw.ei.se.formatters;
 
+import be.uantwerpen.iw.ei.se.models.Permission;
+import be.uantwerpen.iw.ei.se.repositories.PermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+
 /**
  * Created by Thomas on 30/10/2015.
  */
-public class PermissionFormatter {
+@Component
+public class PermissionFormatter implements Formatter<Permission>
+{
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    public Permission parse(final String text, final Locale locale) throws ParseException
+    {
+        if(text != null && !text.isEmpty())
+        {
+            return permissionRepository.findOne(new Long(text));
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public String print(final Permission object, final Locale locale)
+    {
+        return (object != null ? object.getId().toString() : "");
+    }
 }
