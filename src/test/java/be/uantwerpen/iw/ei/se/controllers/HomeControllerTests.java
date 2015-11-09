0e96d9f5e848554d568901473e4637e7b8514b80
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-09 17:30:08 +0100

    Tests cleanup:  - Removed unused imports

--- src/test/java/be/uantwerpen/iw/ei/se/controllers/HomeControllerTests.java
+++ src/test/java/be/uantwerpen/iw/ei/se/controllers/HomeControllerTests.java
@@ -2,24 +2,16 @@ package be.uantwerpen.iw.ei.se.controllers;
 
 import be.uantwerpen.iw.ei.se.models.User;
 import be.uantwerpen.iw.ei.se.services.UserService;
-import com.sun.scenario.effect.impl.sw.java.JSWBlend_EXCLUSIONPeer;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
