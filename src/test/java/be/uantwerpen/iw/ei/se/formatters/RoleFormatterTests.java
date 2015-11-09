6f6455c0318bd5fbecb415e7a40d1eca19b1defa
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-09 18:12:10 +0100

    Added testclass: + RoleFormatterTests

---
+++ src/test/java/be/uantwerpen/iw/ei/se/formatters/RoleFormatterTests.java
@@ -0,0 +1,63 @@
+package be.uantwerpen.iw.ei.se.formatters;
+
+import be.uantwerpen.iw.ei.se.ClickOMaticApplication;
+import be.uantwerpen.iw.ei.se.models.Role;
+import be.uantwerpen.iw.ei.se.repositories.RoleRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by Thomas on 09/11/2015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClickOMaticApplication.class)
+@WebAppConfiguration
+public class RoleFormatterTests
+{
+    @Autowired
+    RoleFormatter roleFormatter;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    Role role;
+    Locale locale;
+
+    @Before
+    public void init()
+    {
+        role = new Role("formatterTest");
+
+        roleRepository.save(role);
+
+        locale = new Locale("default");
+    }
+
+    @Test
+    public void parserTest() throws ParseException
+    {
+        Role parserRole = new Role();
+
+        parserRole = roleFormatter.parse(role.getId().toString(), locale);
+
+        assertTrue(parserRole.equals(role));
+    }
+
+    @Test
+    public void toStringTest()
+    {
+        String parsedString = roleFormatter.print(role, locale);
+
+        assertTrue(parsedString.equals(role.getId().toString()));
+    }
+}
