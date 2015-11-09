298557a06b046ad02ec17c03b02aa2a73a913fc3
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-09 18:21:37 +0100

    Added javascript class:  + FittsCircle

---
+++ src/main/resources/static/js/fittsTest/FittsCircle.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Thomas on 05/11/2015.
+ */
+function FittsCircle(size, posX, posY)
+{
+    this.size = size;
+    this.posX = 0;
+    this.posY = 0;
+    this.color = "blue";
+    this.dashLength = 1;
+
+    this.draw = function(context)
+    {
+        context.beginPath();
+
+        //Draw circle
+        context.arc(this.posX, this.posY, this.size, 0, 2 * Math.PI);
+
+        context.stroke();
+    }
+
+    this.calculateCirclePoints = function()
+    {
+        var n = this.size/this.dashLength;
+        var alpha = Math.PI * 2 / dots;
+        var pointObject = {};
+        var points = [];
+        i = -1;
+    }
+}
