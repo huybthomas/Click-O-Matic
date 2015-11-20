6fcf95866e5de0ad94ad4fe17edd76e23c834241
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-20 20:34:21 +0100

    Patch:  - Cursor position (Y) was not properly calculated for the dotts, user had to click 1/3 canvas height above the targets to progress [Fixed]

--- src/main/resources/static/js/fittsTest/FittsTestStage.js
+++ src/main/resources/static/js/fittsTest/FittsTestStage.js
@@ -154,9 +154,9 @@ function FittsTestStage(numberOfDots, dotRadius, dotDistance)
 
     this.checkTargetClicked = function()
     {
-        //Temporary conversion from cursor position (relative to test center) relative to the upper left corner of the canvas (until coordinates are standardized to test center
+        //Temporary conversion from cursor position (relative to test center) relative to the upper left corner of the canvas (until coordinates are standardized to test center)
         var tempPosX = this.cursorState.x + (canvas.width)/2;
-        var tempPosY = this.cursorState.y + (canvas.height)/2;
+        var tempPosY = this.cursorState.y + (canvas.height)/3;
 
         if(this.dotsList[this.nextTarget].cursorOver(tempPosX, tempPosY))
         {
