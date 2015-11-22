1b120f19c52d6cbffd566714df1d851757c6b9d6
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-22 23:30:29 +0100

    Patch corrupted test: RegistrationControllerTests

--- src/test/java/be/uantwerpen/iw/ei/se/controllers/RegistrationControllerTests.java
+++ src/test/java/be/uantwerpen/iw/ei/se/controllers/RegistrationControllerTests.java
@@ -1,6 +1,8 @@
 package be.uantwerpen.iw.ei.se.controllers;
 
 import be.uantwerpen.iw.ei.se.models.User;
+import be.uantwerpen.iw.ei.se.models.Role;
+import be.uantwerpen.iw.ei.se.services.RoleService;
 import be.uantwerpen.iw.ei.se.services.UserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +11,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,17 +27,23 @@ public class RegistrationControllerTests
     @Mock
     private UserService userService;
 
+    @Mock
+    private RoleService roleService;
+
     @InjectMocks
     private RegistrationController registrationController;
 
     private MockMvc mockMvc;
 
     User principalUser;
+    Iterable<Role> roleList;
 
     @Before
     public void setup()
     {
         principalUser = new User("Test", "User");
+
+        roleList = new ArrayList<Role>(Arrays.asList(new Role("Tester")));
 
         MockitoAnnotations.initMocks(this);
 
@@ -43,13 +54,7 @@ public class RegistrationControllerTests
     public void viewCreateUserTest() throws Exception
     {
         when(userService.getPrincipalUser()).thenReturn(principalUser);
-        mockMvc.perform(get("/registration")).andExpect(view().name("mainPortal/registration"));
-    }
-
-    @Test
-    public void viewSubmitNewUserTest() throws Exception
-    {
-        when(userService.getPrincipalUser()).thenReturn(principalUser);
+        when(roleService.findAll()).thenReturn(roleList);
         mockMvc.perform(get("/registration")).andExpect(view().name("mainPortal/registration"));
     }
 }
