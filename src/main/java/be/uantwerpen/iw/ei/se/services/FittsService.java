6ee65777f00f565f2aa026e0dc614a4dec2db0c2
timothy.verstraete <timothy.verstraete@student.uantwerpen.be>
2015-11-22 22:12:01 +0100

    Implementation of create a fittstest everything in place, just a "spring form binding issue" SpringInputGeneralFieldAttrProcessor

--- src/main/java/be/uantwerpen/iw/ei/se/services/FittsService.java
+++ src/main/java/be/uantwerpen/iw/ei/se/services/FittsService.java
@@ -23,4 +23,26 @@ public class FittsService
     {
         return this.fittsRepository.findAll();
     }
+
+    public boolean add(final FittsTest fittstest)
+    {
+        this.fittsRepository.save(fittstest);
+        return true;
+    }
+
+    public void save(FittsTest fittstest)
+    {
+        for(FittsTest u : findAll())
+        {
+            if(u.getId() == fittstest.getId())
+            {
+                u.setNumberOfDots(fittstest.getNumberOfDots());
+                u.setDotSize(fittstest.getDotSize());
+                u.setDotDistance(fittstest.getDotDistance());
+                u.setCompleted(fittstest.getCompleted());
+
+                fittsRepository.save(u);
+            }
+        }
+    }
 }
