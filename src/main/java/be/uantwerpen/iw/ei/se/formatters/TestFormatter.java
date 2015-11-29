4942ad93d1299d77fb4503d158b65e4ee85203f7
Thomas Huybrechts <huybthomas@gmail.com>
2015-11-29 01:48:00 +0100

    Fixed errors in TestFormatter Removed unused imports out of formatter classes:  - TestFormatter  - UserFormatter

--- src/main/java/be/uantwerpen/iw/ei/se/formatters/TestFormatter.java
+++ src/main/java/be/uantwerpen/iw/ei/se/formatters/TestFormatter.java
@@ -1,10 +1,9 @@
 package be.uantwerpen.iw.ei.se.formatters;
 
 import be.uantwerpen.iw.ei.se.fittsTest.models.FittsTest;
-import be.uantwerpen.iw.ei.se.models.User;
 import be.uantwerpen.iw.ei.se.repositories.FittsTestRepository;
-import be.uantwerpen.iw.ei.se.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
@@ -14,7 +13,8 @@ import java.util.Locale;
  * Created by dries on 28/11/2015.
  */
 @Component
-public class TestFormatter {
+public class TestFormatter implements Formatter<FittsTest>
+{
     @Autowired
     private FittsTestRepository fittsTestRepository;
 
@@ -30,7 +30,7 @@ public class TestFormatter {
         }
     }
 
-    public String print(final User object, final Locale locale)
+    public String print(final FittsTest object, final Locale locale)
     {
         return (object != null ? object.getId().toString() : "");
     }
