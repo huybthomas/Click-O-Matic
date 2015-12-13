d2000d41d36cc1918758c16d04129e9eeed1e058
Thomas Huybrechts <huybthomas@gmail.com>
2015-12-13 15:44:22 +0100

    Added mockito JUnit runner to FittsServiceTests

--- src/test/java/be/uantwerpen/iw/ei/se/services/FittsServiceTests.java
+++ src/test/java/be/uantwerpen/iw/ei/se/services/FittsServiceTests.java
@@ -1,7 +1,28 @@
 package be.uantwerpen.iw.ei.se.services;
 
+import be.uantwerpen.iw.ei.se.repositories.FittsTestRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
 /**
  * Created by Thomas on 13/12/2015.
  */
-public class FittsServiceTests {
+@RunWith(MockitoJUnitRunner.class)
+public class FittsServiceTests
+{
+    @InjectMocks
+    private FittsService fittsService;
+
+    @Mock
+    private FittsTestRepository fittsTestRepository;
+
+    @Before
+    public void init()
+    {
+        MockitoAnnotations.initMocks(this);
+    }
 }
